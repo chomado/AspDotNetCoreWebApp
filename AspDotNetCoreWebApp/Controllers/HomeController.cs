627509f5b288e69501e739aadb77fed3be904305
Ayumu Inaba <ainaba@microsoft.com>
2017-11-15 12:27:29 +0900

    ファイルのドラッグアンドドロップ処理

--- AspDotNetCoreWebApp/Controllers/HomeController.cs
+++ AspDotNetCoreWebApp/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace AspDotNetCoreWebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
     }
 }
---
+++ AspDotNetCoreWebApp/wwwroot/js/uploader.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var dropTarget = $('#fileDrop');
+    dropTarget.on('dragover', function (e) {
+        console.log('dragover');
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    dropTarget.on('drop', function (e) {
+        console.log('drop');
+        e.stopPropagation();
+        e.preventDefault();
+        var data = e.originalEvent.dataTransfer;
+
+        handleFile(data.files[0]);
+    });
+
+    function handleFile(file) {
+        debugger;
+    }
+
+
+})();
